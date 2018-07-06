ce5c0eb12d31a3ed5c769126693cafaf6e1c7321
Patrick Kidger <33688385+patrick-kidger@users.noreply.github.com>
2018-07-06 14:12:36 +0100

    rangeinf now recognises numpy.inf as well

--- iterators.py
+++ iterators.py
@@ -6,13 +6,13 @@ _sentinel = object()
 
 
 def rangeinf(start, stop=_sentinel, step=1):
-    """Acts as range, however passing stop=math.inf will have it go forever."""
+    """Acts as range, however passing stop=math.inf or np.inf will have it go forever."""
     # Allows us to pass just one argument and has it act as 'stop', with 'start' set to zero. This is the same
     # behaviour as the usual range function
     if stop == _sentinel:
         stop = start
         start = 0
-    if stop is math.inf:
+    if stop == math.inf:
         return itertools.count(start, step)
     else:
         return range(start, stop, step)
