dd26f46d9c30f44eb7656470c20c018050722031
Patrick Kidger <33688385+patrick-kidger@users.noreply.github.com>
2018-11-19 19:17:44 +0000

    Changed how we avoid referencing multiprocessing until we have to, to aid picklability

--- src/multiprocess.py
+++ src/multiprocess.py
@@ -1,10 +1,7 @@
 import logging
 import sys
 
-from . import modules
-
-
-##### START HACKERY #####
+# START HACKERY #
 
 # The builtin multiprocessing library is a bit weird, in that it offers multiple modes that it can be used in - the
 # correct way to use the library is to import it, and then use it with the correct mode specified, e.g:
@@ -25,7 +22,9 @@ mp_ = None  # Will later be set to the multiprocessing library
 
 
 def set_multiprocessing(mp=None):
-    """Lets tools know that it should use :mp: in place of the multiprocessing library."""
+    """Lets tools know that it should use :mp: in place of the multiprocessing library. Defaults to the normal
+    multiprocessing library.
+    """
 
     global mp_
     if mp_ is not None:
@@ -37,44 +36,67 @@ def set_multiprocessing(mp=None):
     else:
         mp_ = mp
 
-    # Stop making the module callable; put it back to the normal module type
-    _module.__class__ = _module_old_class
+    # Replace the fake ProcessFake base class with the actual multiprocessing.Process class.
+    orig_bases = RedirectedOutputProcess.__bases__
+    ind = orig_bases.index(ProcessFake)
+    new_bases = tuple(mp_.Process if i == ind else x for i, x in enumerate(orig_bases))
+    RedirectedOutputProcess.__bases__ = new_bases
+    # For the record, two other plausible things which don't work here:
+    # - Defining RedirectedOutputProcess dynamically (i.e. wrapping it in a function which takes multiprocessing as an
+    #   argument). This fails to pickle because then it's not defined at the module level.
+    # - Dynamically creating a subclass of RedirectedOutputProcess and multiprocessing.Process in
+    #   RedirectedOutputProcess.__new__ (in a similar way to what tools.Record does) gives a rather esoteric error that
+    #   appears to be directly related to the fact that the resulting instance is not of the expected type.
 
 
-# Just for fun, we make it so that we can __call__ the module to set multiprocessing.
-def __call__(self, mp=None):
-    """Call the module to tell it what to use as the multiprocessing library; it will default to importing the normal
-    multiprocessing library. This is necessary so that multiprocessing can be replaced with the result of
-    multiprocessing.get_context, if necessary.
-    """
-    set_multiprocessing(mp)
+class ProcessFake:
+    def __init__(self, group=None, target=None, name=None, args=(), kwargs=None, *, daemon=None):
+        pass
+
+    def run(self):
+        pass
+
+    def start(self):
+        pass
+
+    def terminate(self):
+        pass
+
+    def join(self, timeout=None):
+        pass
+
+    def is_alive(self):
+        pass
+
+    @property
+    def name(self):
+        return None
+
+    @property
+    def daemon(self):
+        return None
+
+    @property
+    def pid(self):
+        return None
+
+    @property
+    def exitcode(self):
+        return None
+
+    @property
+    def authkey(self):
+        return None
+
+    @property
+    def sentinel(self):
+        return None
 
 
-# Just defining a __call__ function isn't enough; this is where we actually make the module callable.
-_module, _module_old_class = modules.endow_attrs(__name__, {'__call__': __call__})
+# END HACKERY #
 
-
-# Now this is the only class here that needs multiprocessing: in particular, it needs to inherit from
-# multiprocessing.Process. So we simply create a new subclass of it the first time we try to instantiate it.
-class RedirectedOutputProcess:
+class RedirectedOutputProcess(ProcessFake):
     """Redirects the stdout and stderr streams of a Process to the specified :stdout: and :stderr:."""
-
-    _instantiated_class = None
-
-    def __new__(cls, *args, **kwargs):
-        if cls._instantiated_class is None:
-            if mp_ is None:
-                set_multiprocessing()
-            cls._instantiated_class = type(cls.__name__, (cls,  mp_.Process), {})
-        # Just substitute cls for its new subclass cls._instantiated_class.
-        return super(RedirectedOutputProcess, cls._instantiated_class).__new__(cls._instantiated_class)
-
-    def __init_subclass__(cls, **kwargs):
-        # Make sure subclasses recognise that they have to create their own subclass before instantiation
-        cls._instantiated_class = None
-        super(RedirectedOutputProcess, cls).__init_subclass__(**kwargs)
-
-##### END HACKERY #####
 
     def __init__(self, *args, stdout=None, stderr=None, **kwargs):
         """Arguments:
@@ -95,7 +117,7 @@ class RedirectedOutputProcess:
         super(RedirectedOutputProcess, self).run()
 
 
-# Courtesy of # https://github.com/jdloft/multiprocess-logging/blob/master/main.py
+# Courtesy of https://github.com/jdloft/multiprocess-logging/blob/master/main.py
 class StreamToLogger:
     """Converts streams (stdout or stderr for example) to logs."""
 
